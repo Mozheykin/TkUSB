0df49c688070da72802fa4d45ab4acf33cd735fa
Mozheykin <Mozheykin'@example.com>
2022-12-07 15:42:19 +0300

    change other_sync

--- other_sync.py
+++ other_sync.py
@@ -1,5 +1,3 @@
-import createObjects
-from functions import change_collor
 from ctypes import windll
 from loguru import logger
 
@@ -8,9 +6,18 @@ class Sync:
     LIBRARY = 'dll\\USBaccessX64.dll'
 
     _devCnt = {
-        0: 'USB Multiplexer 2x4',
-        1: 'USB 3.0 Connect',
-        2: 'USB Switch 4',
+        'USB Switch 4': {
+            'devType': 8,
+            'devVersion': 513,
+        },
+        'USB 3.0 Connect': {
+            'devType': 8,
+            'devVersion': 259,
+        },
+        'USB Multiplexer 2x4': {
+            'devType': 48,
+            'devVersion': 129,
+        }
     }
 
     def __init__(self, root) -> None:
@@ -29,8 +36,13 @@ class Sync:
                 devType = self.dll.FCWGetUSBType(self.cw, devNum)
                 devVersion = self.dll.FCWGetVersion(self.cw, devNum)
 
+                devCnt = None
+                for name, pr in Sync._devCnt:
+                    if all([pr['devType'] == devType, pr['devVersion'] == devVersion]):
+                        devCnt = name
+
                 self.devices[devNum] = {
-                    'devCnt': Sync._devCnt[devNum],
+                    'devCnt': devCnt,
                     'serNum': serNum,
                     'devType': devType,
                     'devVersion': devVersion,
@@ -65,11 +77,13 @@ class Sync:
         return 0
 
     def set_pin_param(self, serNum, PIN:str=None, value:int=0, PINS:list=[], values:list=[]) -> bool:   
-        if all([devNum:=self.verify_serNum(serNum), PIN]):
+        devNum = self.verify_serNum(serNum)
+        logger.info(f'set_pin_param() {devNum}, {PIN}')
+        if all([not devNum is None, PIN]):
             self.dll.FCWSetSwitch(self.cw, devNum, 0x10 + self.split_pin_for_set(PIN), value)
             logger.info(f'change_pin_param() {devNum}, {serNum}, {PIN}, {value}')
             return True
-        elif all([devNum:=self.verify_serNum(serNum), PINS]):
+        elif all([not devNum is None, PINS]):
             [self.dll.FCWSetSwitch(self.cw, devNum, 0x10 + self.split_pin_for_set(PIN), value) for PIN, value in zip(PINS, values)]
             return True
         return False
@@ -77,7 +91,7 @@ class Sync:
     def get_pin_param(self, serNum, PIN:str=None, PINS:list=[]) -> int | list | None:
         devNum, params =self.verify_serNum(serNum, take_params=True, update=True) # Update devices in 52 line
         logger.info(f'get_pin_param() {devNum}, {params}')
-        if devNum:
+        if not devNum is None:
             if PIN:
                 result = params.get(PIN)
                 logger.info(f'get_pin_param() {devNum}, {serNum}, {result}')
@@ -88,7 +102,8 @@ class Sync:
                 return result 
     
     def set_multi_switch(self, serNum, contact) -> bool:
-        if devNum:=self.verify_serNum(serNum, update=True):
+        devNum = self.verify_serNum(serNum, update=True)
+        if not devNum is None:
             value = 2 ** contact - 1
             self.dll.FCWSetMultiSwitch(self.cw, devNum, value)
             logger.info(f'set_multi_swirch() {devNum}, {serNum}, {value}')
@@ -96,15 +111,8 @@ class Sync:
         return False
     
     def get_multi_switch(self, serNum:int, mask:int=0, value:int=0, seqNum:int=0) -> int | None:
-        '''int GetMultiSwitch(int deviceNo, unsigned long int *mask, unsigned long int *value, int seqNumber) ;
-           int FCWGetMultiSwitch(CUSBaccess *obj, int deviceNo,...);
-
-        All input channels of the device USB-IO16 could be scanned with the command
-        “GetMultiSwitch”. The argument “mask” indicates the channels which changes the values
-        since the last call of GetMultiSwitch. Channel 0 is the least significant bit (LSB). The
-        argument shows the current status of all channels. The argument “seqNum” should be set
-        to 0. Please note: The USB-Contact is polled using GetSwitch(..).'''
-        if devNum:=self.verify_serNum(serNum, update=True):
+        devNum = self.verify_serNum(serNum, update=True)
+        if not devNum is None:
             # mask, and value is optional values
             result = self.dll.FCWGetMultiSwitch(self.cw, devNum, seqNum)
             # if use mask, values
@@ -119,25 +127,36 @@ class Sync:
 def test():
     try:
         sync = Sync(root=None)
-        _device_for_selected = sync.update_all_devices() # get all device in dict
-        sync.get_list_devices() # get list device for combobox
-        # first device get PIN1 and two PIN3, PIN5 for 0 device
+        _device_for_selected = sync.update_all_devices()  # get all devices in dict
+        sync.get_list_devices()  # get list device for combobox
+        # first device get PIN1 and two PIN3, PIN4 for 0 device
         select0_device = _device_for_selected[0]
         serNum0_device = select0_device['serNum']
+        sync.set_multi_switch(serNum=serNum0_device, contact=4)
+        sync.get_multi_switch(serNum=serNum0_device)
+
         sync.get_pin_param(serNum=serNum0_device, PIN='PIN_1')
-        sync.get_pin_param(serNum=serNum0_device, PINS=['PIN_3', 'PIN_5'])
-        # second devise set PIN2 and two PIN5, PIN6 for 1 device
+        sync.get_pin_param(serNum=serNum0_device, PINS=['PIN_3', 'PIN_4'])
+        # second device set PIN2 and two PIN3, PIN4 for 0 device
+        sync.set_pin_param(serNum=serNum0_device, PIN='PIN_2', value=1)
+        sync.set_pin_param(serNum=serNum0_device, PINS=['PIN_3', 'PIN_4'], values=[1, 1])
+
+        # third device set PIN2 and two PIN5, PIN6 for 1 device
         select1_device = _device_for_selected[1]
         serNum1_device = select1_device['serNum']
         sync.set_pin_param(serNum=select1_device, PIN='PIN_2', value=1)
         sync.set_pin_param(serNum=serNum1_device, PINS=['PIN_5', 'PIN_6'], values=[1, 1])
-        # therd devise set all PINS 1 for 2 device
+        # fourth device set all PINS 1 for 2 device
         select2_device = _device_for_selected[2]
         serNum2_device = select2_device['serNum']
-
-        # contact = 4
-        sync.set_multi_switch(serNum=serNum2_device, contact=4)
-        # four device get all PINS for 1 device
+        sync.set_multi_switch(serNum=serNum2_device, contact=8)
+        sync.get_multi_switch(serNum=serNum2_device)
+        sync.set_multi_switch(serNum=serNum2_device, contact=0)
+        sync.get_multi_switch(serNum=serNum2_device)
+        sync.set_multi_switch(serNum=serNum2_device, contact=8)
+        sync.get_multi_switch(serNum=serNum2_device)
+        # fifth device get all PINS for 1 device
+        sync.set_multi_switch(serNum=serNum1_device, contact=1)
         sync.get_multi_switch(serNum=serNum1_device)
         sync.close()
     except Exception as ex:
