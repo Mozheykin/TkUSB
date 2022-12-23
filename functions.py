d7fcc9f133ec0fb5420f25e6580cbaa87283d3f6
Mozheykin <Mozheykin'@example.com>
2022-12-23 11:56:58 +0300

    delete old files

--- functions.py
+++ functions.py
@@ -1,51 +1,82 @@
-from tkinter import PhotoImage, Button 
+from classes import Objects
+from tkinter import PhotoImage, Button, CENTER
 from functools import partial
-from loadConfiguration import save_configurations
-from classes import Objects, _Combobox
-from loguru import logger
+from pprint import pprint
 
 
-COLLOR0, COLLOR1 = 'red', 'green'
-MAIN_COLLOR = 'gray'
+def all_on(root, name:str, _type:str) -> None:
+    objects_ = Objects.objects_on_the_panel.get(_type)
+    object_ = objects_.get(name)
+    on_what = object_.on_what
+    for name, object_ in objects_.items():
+        if object_.on_what == on_what:
+            object_ = object_._replace(activate = 1)
+            object_.object_['bg'] = object_.collors[1]
+            object_.object_['text'] = object_.text_swich[1]
+            Objects.objects_on_the_panel[_type][name] = object_
 
 
-def all_on(root, name:str, _type:str):
-    buttons = Objects.objects_on_the_panel.get('buttons')
-    for button in buttons.values():
-        button[0].config(bg=COLLOR1)
+def all_off(root, name:str, _type:str) -> None:
+    objects_ = Objects.objects_on_the_panel.get(_type)
+    object_ = objects_.get(name)
+    on_what = object_.on_what
+    for name, object_ in objects_.items():
+        if object_.on_what == on_what:
+            object_ = object_._replace(activate = 0)
+            object_.object_['bg'] = object_.collors[0]
+            object_.object_['text'] = object_.text_swich[0]
+            Objects.objects_on_the_panel[_type][name] = object_
 
 
-def all_off(root, name:str, _type:str):
-    buttons = Objects.objects_on_the_panel.get('buttons')
-    for button in buttons.values():
-        button[0].config(bg=COLLOR0)
+def change_collor(root, name:str, _type:str, collor:str=None) -> None:
+    objects_ = Objects.objects_on_the_panel.get(_type)
+    object_ = objects_.get(name)
+    state = {0:1, 1:0}
+    object_ = object_._replace(activate=state[object_.activate])
+    object_.object_['bg'] = object_.collors[object_.activate]
+    object_.object_['text'] = object_.text_swich[object_.activate]
+    interaction = Objects.objects_on_the_panel['comboboxs'].get(object_.interaction)
+    if interaction:
+        select_value = interaction.object_.get()
+        if select_value:
+            ind, serNum, devType, devCnt = select_value.split(':')
+            pin_val = Objects.dll.get_pin_param(serNum=serNum, PIN=object_.saved)
+            if pin_val != object_.activate:
+                Objects.dll.set_pin_param(serNum=serNum, PIN=objects_.saved, value=object_.activate)
+    for any_name, any_object_ in objects_.items():
+        if object_.saved == any_object_.saved:
+            any_object_ = any_object_._replace(activate = object_.activate)
+            any_object_.object_['bg'] = object_.collors[object_.activate]
+            any_object_.object_['text'] = object_.text_swich[object_.activate]
+            Objects.objects_on_the_panel[_type][any_name] = any_object_
+    Objects.objects_on_the_panel[_type][name] = object_
 
 
-def change_collor(root, name:str, _type:str, operation:str='',collor:str=None) -> None:
-    combobox:_Combobox = Objects.objects_on_the_panel['comboboxs'].get('CB1')
-    # TODO get parametrs with class _Button in parametr interaction
-    select_value = ''
-    serNum = ''
-    if combobox:
-        select_value = combobox.object_.get()
-        for value in combobox.values:
-            if value[1] == select_value:
-                serNum = value[0]
-    logger.info('change_collor()'.center(75, '='))
-    logger.info(f'dll: {Objects.dll}')
-    logger.info(f'Objects {Objects.objects_on_the_panel}')
-    logger.info(f'Select_value: {select_value}, serNum: {serNum}')
-    type_object = Objects.objects_on_the_panel.get(_type)
-    _object = type_object.get(name)
-    if not collor:
-        if _object[0]['bg'] == COLLOR0:
-            logger.info(f'set_pin_param -> Enable: {Objects.dll.set_pin_param(serNum=serNum,PIN=name,value=1)}')
-            _object[0].config(bg=COLLOR1)
-        else:
-            logger.info(f'set_pin_param -> Disable: {Objects.dll.set_pin_param(serNum=serNum,PIN=name,value=0)}')
-            _object[0].config(bg=COLLOR0)
-    else:
-        _object[0].config(bg=collor)
+def change_checkbutton_position(root, name:str, _type:str) -> None:
+    objects_ = Objects.objects_on_the_panel.get(_type)
+    object_ = objects_.get(name)
+    state = {0:1,1:2,2:0}
+    act = state[object_.act]
+    object_ = object_._replace(act=act)
+    images = object_.images
+    activate=f'{object_.type_}-{images[act]}'
+    object_ = object_._replace(activate=activate)
+    img_button = get_picture(object_.activate, object_.subsample, object_.subsample)
+    x, y = get_position_object(object_.object_)
+    object_.object_.destroy()
+    object_ = object_._replace(object_ = Button(
+        root,
+        image=img_button,
+        command=partial(COMMANDS[object_.command], root, name, 'img_buttons'),
+        bd=object_.bd,
+        highlightthickness=object_.highlightthickness,
+        activebackground=object_.activebackground,
+        bg=object_.bg
+    ))
+    object_.object_.image = img_button
+    object_.object_.pack(pady=5)
+    object_.object_.place(relx=object_.relx, rely=object_.rely, anchor=CENTER)
+    Objects.objects_on_the_panel[_type][name] = object_
 
 
 def get_picture(name_button:str, change_x:int, change_y:int) -> PhotoImage:
@@ -53,36 +84,8 @@ def get_picture(name_button:str, change_x:int, change_y:int) -> PhotoImage:
     img = img.subsample(change_x, change_y)
     return img
 
-
 def get_position_object(_object):
     return _object.winfo_x(), _object.winfo_y()
-
-
-def change_checkbutton_position(root, name:str, _type:str) -> None:
-    type_object = Objects.objects_on_the_panel.get(_type)
-    _object = type_object.get(name)[0]
-    actuall_button = None
-    match type_object.get(name)[1]:
-        case 'yes':
-            img = get_picture(f'{type_object.get(name)[2]}-no', 6, 6)
-            actuall_button = 'no'
-        case 'no':
-            img = get_picture(f'{type_object.get(name)[2]}-null', 6, 6)
-            actuall_button = 'null'
-        case 'null':
-            actuall_button = 'yes'
-            img = get_picture(f'{type_object.get(name)[2]}-yes', 6, 6)
-    img_button = Button(root, image=img, command=partial(COMMANDS['change_checkbutton_position'],root, name,_type), bd=0, highlightthickness=0, 
-    activebackground='gray', bg='gray')
-    x, y = get_position_object(_object)
-    _object.destroy()
-    img_button.image = img
-    img_button.pack(pady=5)
-    img_button.place(x=x, y=y)
-    Objects.objects_on_the_panel[_type][name] = [img_button, actuall_button, type_object.get(name)[2]]
-
-
-logger.add('functions.log', format='{time} {level} {message}', level='INFO')
 
 COMMANDS = {
     'all_on': all_on,
